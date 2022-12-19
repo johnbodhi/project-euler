eb389d2725e393f8832659d0239ad9264c5941a8
johnbodhi <61892807+johnbodhi@users.noreply.github.com>
2022-12-19 17:05:56 -0600

    Update

--- Project_Euler_Problem_18/MaxPathV3/MaxPathV3/MaxPathV3.f90
+++ Project_Euler_Problem_18/MaxPathV3/MaxPathV3/MaxPathV3.f90
@@ -40,8 +40,9 @@ program MaxPathV3
         do i = 1,N,1            
             temp = A(i,M-j+1);
             A(i,M-j+1) = A(i,j);
-            A(i,j) = temp;           
+            A(i,j) = temp;
         enddo
+        V(j) = 1;
     enddo            
     
     ! Accumulate while limit...
@@ -57,12 +58,14 @@ program MaxPathV3
     jj = 1; uu = 1; ii = 0;
     do while( uu .le. kk )
     
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         ii = ii + 1;        
         V(ii) = V(ii) + 1; 
-        
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
         do j = 1,M,1        
-            S = S + A(V(j),j); 
-        enddo   
+            S = S + A(V(j),j);  
+        enddo  
         
         B(2) = S;
         
@@ -110,42 +113,6 @@ program MaxPathV3
         uu = uu + 1;
         S = 0;        
     enddo
-    
-    ! Accumulate while limit...
-    do j = 1,size(V)-2,1
-        do i = 1,size(V)-j,1        
-            kk = kk + 1;        
-        enddo
-    enddo
-    
-    ! Gamma...    
-    jj = 1; uu = 1; ii = 0;
-    do while( uu .le. kk )
-    
-        ii = ii + 1;        
-        V(ii) = V(ii) + 1; 
-        
-        do j = 1,M,1        
-            S = S + A(V(j),j); 
-        enddo   
-        
-        B(2) = S;
-        
-        if( B(2) > B(1) ) then
-        
-            B(1) = S; B(2) = 0        
-        else        
-            B(2) = 0        
-        endif   
-        
-        if( ii .eq. size(V)-jj ) then
-            ii = 0;
-            jj = jj + 1;      
-        endif
-        
-        uu = uu + 1;
-        S = 0;        
-    enddo    
     
     ! Print maximum path...
     print*, B(1)
