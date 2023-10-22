590a59b5608b79e38bafa541d5dd98f8a412acab
johnbodhi <JMGarofalo@gmail.com>
2023-10-22 15:51:32 -0700

    Update

---
+++ Project_Euler_Problem_18/MaxPathII.m
@@ -0,0 +1,141 @@
+clear all; close all; clc; tic
+
+% We can read in a triangle froma file, or generate a triangle of arbitrary
+% size to find a path from the top to bottom which yields a maximum sum for
+% any lowwr trianglular matrix containing random numbers...
+
+% A = [ 3 0 0 0; 7 4 0 0; 2 4 6 0; 8 5 9 3 ]; 
+A = readmatrix("triangle_small.csv"); 
+% A = csvread("triangle_large.csv"); 
+
+N = size(A,1); M = size(A,2);
+
+% N = 50; M = N;
+% for j = 1:M
+%     for i = 1:N
+%         if( i < j || i == j)
+%             A( i, j ) = randi( [ 10,99 ] );
+%             B( i, j ) = i;
+%         end
+%     end
+% end
+
+% Flip matrix A to take take advantage of symmetry.
+
+AS = zeros(size(A,1),size(A,2));
+
+for i = 1:1:size(A,2)
+
+    AS(i,1:i) = flip(A(i,1:i),2);
+end
+
+% Transpose matrix A to march through natrually.
+
+A  = A';
+AS = AS';
+
+% Generate 3-D storage of all branch subspaces within decision constraint. 
+
+D = zeros( size(A,1), size(A,2), size(A,1) ); kk = 1;
+
+for k = size( D, 3 ):-1:1
+    for j = 1:1:size( D, 2 )
+        for i = 1:1:size( D, 1 )               
+            
+            if( k == size(D,3) )
+                cc = 0;
+                for j = 1:size(D,2)
+                    for i = 1:size(D,1)
+                        if( i == j )
+                            D( k - cc, j, k ) = k - cc;
+                            cc = cc + 1;
+                        end
+                    end
+                end
+
+            elseif( k < size(D,2) || k > 1 )
+
+                hh = size(D,2) - k + 1; 
+                dd = 1;
+                for p = k:-1:1
+                    D( p, dd:hh, k ) = p;
+                    dd = dd + 1;
+                    hh = hh + 1; 
+                end                
+
+            elseif( k == 1 )
+                    
+                for j = 1:size(D,2)
+                    for i = 1:size(D,1)
+                        if( i == size(D,1) )
+                            D( size(D,1), j, k ) = k;
+                        end
+                    end
+                end
+                
+            end
+        end
+    end
+end
+
+% Re-assign all true values in matrix A associated with the Trellis 
+% indexes to matrix R.
+
+D = flip(D,2);
+for k = 1:size(D,3)
+    for j = 1:size(D,2)
+        for i = 1:size(D,1)
+            if( D( i, j, k ) )
+
+                RA( i, j, k ) = A( D( i, j, k ), j );
+                RS( i, j, k ) = AS( D( i, j, k ), j );
+            end            
+        end
+    end
+end
+
+% We want to march through the first half of the triangle, and it's
+% reflection to minimize the search for eigenvectors that define the 
+% paths through the dyadic tree.
+
+if( mod(N,2) ~= 0 )
+
+    R = cat(3,RA(:,:,1:floor(N/2)+1),RS(:,:,1:floor(N/2)));
+    R(:,:,floor(N/2)+2:N) = flip(R(:,:,floor(N/2)+2:N),3);
+else
+
+    R = cat(3,RA(:,:,1:floor(N/2)),RS(:,:,1:floor(N/2)));
+    R(:,:,floor(N/2)+1:N) = flip(R(:,:,floor(N/2)+1:N),3);
+end
+D = flip(D,2); R = flip(R,2);
+
+% Find all legal paths through the Trellis.
+
+Z = diag( flip( pascal( size( A, 1 ) ), 2 ) ); % Number of paths through the triangle from each bottom index.
+
+% for i = 1:1:isze(Z,1)
+% 
+%     while( X <= Z(i,1) )
+% 
+%         B = permn([0 1], N, k);
+% 
+% 
+% 
+%     end
+% 
+% end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
