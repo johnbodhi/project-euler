f9450a52a7bac282ace8cf5d8565742e808422ca
johnbodhi <61892807+johnbodhi@users.noreply.github.com>
2024-11-13 22:54:48 -0700

    Update genFun.m

--- Project_Euler_Problem_67/MaximumPathSumII/MaximumPathSumII/MATLAB/genFun.m
+++ Project_Euler_Problem_67/MaximumPathSumII/MaximumPathSumII/MATLAB/genFun.m
@@ -1,6 +1,6 @@
 function [ V_, B_, K_, P_, G_, EXIT_ ] = genFun( N_, Q_, K_, P_ )
 
-    SUP = 1e2; EXIT_ = 0;
+    SUP = 1e3; EXIT_ = 0;
 
     V_  = zeros(N_-1,SUP,ceil(N_/2)); V_(:,1,:) = Inf;
     
