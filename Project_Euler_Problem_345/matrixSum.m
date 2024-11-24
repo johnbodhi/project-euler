f4ec32b43dbf443c32d9bd65dcd3009a0b30726a
johnbodhi <61892807+johnbodhi@users.noreply.github.com>
2024-11-24 16:55:41 -0700

    Update

--- Project_Euler_Problem_345/matrixSum.m
+++ Project_Euler_Problem_345/matrixSum.m
@@ -1,9 +1,16 @@
 clear all; close all; clc; tic
 
+A = readmatrix("C:\Users\jmgar\OneDrive\Documents\GitHub\project-euler\Project_Euler_Problem_345\matrixSum.csv");
 
+%{
+N = 10; M = N;
+for j = 1:M
+    for i = 1:N
 
-
-
+        A( i, j ) = randi( [ 100, 999 ] );
+    end
+end
+%}
 
 
 
