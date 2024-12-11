fe5ab786deb524fa74e0c5ce9b3150b2ff436a0d
johnbodhi <61892807+johnbodhi@users.noreply.github.com>
2024-12-11 00:10:02 -0700

    Update

--- Project_Euler_Problem_67/MaximumPathSumII/MaximumPathSumII/MATLAB/monteCarlo.m
+++ Project_Euler_Problem_67/MaximumPathSumII/MaximumPathSumII/MATLAB/monteCarlo.m
@@ -1,8 +1,10 @@
 function [ B_ ] = monteCarlo( N_, EMAX_ )
 
-    OPT = floor( 2^EMAX_ / 2^52 );
-    
-    K_ = randi( [0, OPT] ) * randi( [0, 2^52 ] );
+    K_ = randi( [2, floor(2^EMAX_)] );
+
+    % OPT = floor( 2^EMAX_ / 2^52 );
+     
+    % K_ = randi( [0, OPT] ) * randi( [0, 2^52 ] );
 
     B_ = permn( [0;1], N_-1, K_ );
 
