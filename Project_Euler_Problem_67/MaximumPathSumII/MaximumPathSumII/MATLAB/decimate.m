130aa5bf086ce77a0fb9309c717b5bcb96710f5e
johnbodhi <61892807+johnbodhi@users.noreply.github.com>
2025-08-07 04:48:45 -0700

    Update

--- Project_Euler_Problem_67/MaximumPathSumII/MaximumPathSumII/MATLAB/decimate.m
+++ Project_Euler_Problem_67/MaximumPathSumII/MaximumPathSumII/MATLAB/decimate.m
@@ -1,18 +1,16 @@
-function [ RA_,RAF_,RS_,RSF_ ] = decimate(RA_, RAF_, RS_, RSF_ )
+function [ P_ ] = decimate(RA_,RS_)
 
     % Cut and unjoin each slice of each trellis to flip and swap in the
     % contraction. We transform each slice, and decimate contiguously 
-    % until we achieve a many-one reduction.
-
+    % until we achieve a many-one reduction in exponential space, and
+    % exponential time.
+    
     RA_  = circshift(RA_,1,1);
     RAF_ = flip(RA_,2);
     RAF_ = swap(RAF_);
-
+    
     RS_  = circshift(RS_,1,1);    
     RSF_ = flip(RS_,2);
     RSF_ = swap(RSF_);
-
-
     
-
-end
+    
