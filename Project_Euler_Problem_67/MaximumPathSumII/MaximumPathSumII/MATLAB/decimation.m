89eb8d36d69e9cd849ad491bc6b5184671ba3d90
johnbodhi <61892807+johnbodhi@users.noreply.github.com>
2025-09-03 00:40:29 -0700

    Update

--- Project_Euler_Problem_67/MaximumPathSumII/MaximumPathSumII/MATLAB/decimation.m
+++
@@ -1,3 +0,0 @@
-function [ NEXPSPACE_ ] = decimation(A_)
-
-    
--- Project_Euler_Problem_67/MaximumPathSumII/MaximumPathSumII/MATLAB/trellis.m
+++ Project_Euler_Problem_67/MaximumPathSumII/MaximumPathSumII/MATLAB/trellis.m
@@ -1,23 +1,16 @@
-function [NEXSPACE] = trellis()
+function [EXSPACE] = trellis()
 
-    [A_]        = LTM()             ;
+    [A_]         = LTM()                     ;
 
-    [D_]        = scaffold(A_)      ;
+    [D_]         = scaffold(A_)              ;
 
-    [RAA_,RSA_] = mapScaffold(A_,D_);
+    [RAA_,RSA_]  = mapScaffold(A_,D_)        ;
 
-    RAB_ = flip(RAA_,2); RSB_ = flip(RSA_,2);
-    RAC_ = smudge(RAB_); RSC_ = smudge(RSB_);
-    RAD_ = swap(RAC_,1); RSD_ = swap(RSC_,1);
-    RAE_ = swap(RAD_,2); RSE_ = swap(RSD_,2);
+    [R_]         = transform(RAA_,RSA_)      ;
 
-    R_(:,:,:,1)  = RAA_; R_(:,:,:,2)  = RSA_;
-    R_(:,:,:,3)  = RAB_; R_(:,:,:,4)  = RSB_;
-    R_(:,:,:,5)  = RAC_; R_(:,:,:,6)  = RSC_;
-    R_(:,:,:,7)  = RAD_; R_(:,:,:,8)  = RSD_;
-    R_(:,:,:,9)  = RAE_; R_(:,:,:,10) = RSE_;
-
-    EXPSPACE     = foldLeaves(R_); save("NEXPSPACE.mat","NEXPSPACE_");
+    EXPSPACE     = decimate(R_)              ; 
+    
+    save("NEXPSPACE.mat","NEXPSPACE_")  ;
 
     N_           = load("N_.mat")       ; 
     N_           = N_.N_                ;
