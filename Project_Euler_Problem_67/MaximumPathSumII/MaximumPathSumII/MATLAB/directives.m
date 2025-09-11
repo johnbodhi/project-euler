0f9a26aa048db3110c89981f289d52fff7264915
johnbodhi <61892807+johnbodhi@users.noreply.github.com>
2025-09-11 14:27:41 -0700

    Update

--- Project_Euler_Problem_67/MaximumPathSumII/MaximumPathSumII/MATLAB/directives.m
+++ Project_Euler_Problem_67/MaximumPathSumII/MaximumPathSumII/MATLAB/directives.m
@@ -1,9 +1,13 @@
 function [B_,K_] = directives(N_,K_,EMAX_)
 
-    B_(1,:) = permn([0;1],N_,K_) ; K_ = K_ + 1;
+    % M_          = 2^(K_-1)+1        ;
 
-    B_(2,:) = flip(B_(1,:),2)    ;
+    % B_(1,:)     = permn([0;1],N_,M_); K_ = K_ + 1;
 
-    % B_(3,:) = monteCarlo(N_,EMAX_);
+    B_(1,:)     = permn([0;1],N_,K_); K_ = K_ + 1;
     
-    % B_(5:6,:) = DNN();
+    B_(2,:)     = flip(B_(1,:),2)   ;
+
+    % B_(3,:)   = monteCarlo(N_,EMAX_);
+    
+    % B_(5:6,:) = DNN( )              ;
--- Project_Euler_Problem_67/MaximumPathSumII/MaximumPathSumII/MATLAB/pAdicDT.m
+++ Project_Euler_Problem_67/MaximumPathSumII/MaximumPathSumII/MATLAB/pAdicDT.m
@@ -9,26 +9,26 @@ function [F_] = pAdicDT(F_,B_,R_)
         k = sum(B_(i,:))+1; ii = 1; jj = 1;
 
         for j = 1:1:N  
-            if ( ~B_(i,j)   )         
-                for l = 1:1:M
+            if ( ~B_(i,j)   )   
+                for l = M:-1:1
                                                     
-                    S_(l) = S_(l) + RT_ (ii,jj,k,l);
-                    S_(l) = S_(l) + RB_ (ii,jj,k,l);
-                    S_(l) = S_(l) + RTS_(ii,jj,k,l);
-                    S_(l) = S_(l) + RBS_(ii,jj,k,l);
-                end                
+                    S_(l)   = S_(l)   + RT_ (ii,jj,k,l);
+                    S_(l-1) = S_(l-1) + RB_ (ii,jj,k,l);
+                    S_(l-2) = S_(l-2) + RTS_(ii,jj,k,l);
+                    S_(l-3) = S_(l-3) + RBS_(ii,jj,k,l);
+                end             
                 jj = jj + 1;                            
             elseif ( B_(i,j) )                             
-                for l = 1:1:M
-                    
-                    S_(l) = S_(l) + RT_ (ii,jj,k,l);
-                    S_(l) = S_(l) + RB_ (ii,jj,k,l);
-                    S_(l) = S_(l) + RTS_(ii,jj,k,l);
-                    S_(l) = S_(l) + RBS_(ii,jj,k,l);
-                end                
+                for l = M:-1:1
+
+                    S_(l)   = S_(l)   + RT_ (ii,jj,k,l);
+                    S_(l-1) = S_(l-1) + RB_ (ii,jj,k,l);
+                    S_(l-2) = S_(l-2) + RTS_(ii,jj,k,l);
+                    S_(l-3) = S_(l-3) + RBS_(ii,jj,k,l);
+                end    
                 jj = jj + 1;
-                ii = ii + 1;               
-            end        
+                ii = ii + 1;              
+            end     
         end
-        [F_] = sol(S_,F_); [S_] = resetAcc(R_);    
+        [F_] = sol(S_,F_); [S_] = resetAcc(R_); 
     end
