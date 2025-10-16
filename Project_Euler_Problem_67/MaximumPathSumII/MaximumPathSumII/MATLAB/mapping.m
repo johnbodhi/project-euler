d377cea3f6a93698bd7278669381ea7cc1afa4e3
johnbodhi <61892807+johnbodhi@users.noreply.github.com>
2025-10-16 16:50:45 -0700

    Update

--- Project_Euler_Problem_67/MaximumPathSumII/MaximumPathSumII/MATLAB/mapping.m
+++ Project_Euler_Problem_67/MaximumPathSumII/MaximumPathSumII/MATLAB/mapping.m
@@ -1,27 +1,31 @@
 function [R_] = mapping(R_,D_)
                      
-    N_        = size(R_,1)         ; 
+    N_            = size(R_,1)                ; 
 
-    RA(:,:,1) = R_                 ;    
-    RS(:,:,1) = swap(R_,2)         ;
-                     
-    RA(:,:,1) = RA(:,:,1)'         ;
-    RS(:,:,1) = RS(:,:,1)'         ;
+    for l = 1:1:size(R_,4)
 
-    RA_  = zeros(N_,N_,ceil(N_/2)) ;
-    RS_  = zeros(N_,N_,ceil(N_/2)) ;     
+        RA(:,:,:) = R_(:,:,:,l)               ;    
+        RS(:,:,:) = swap(RA,2)                ;
+                         
+        RA(:,:,:) = pagetranspose(RA)         ;
+        RS(:,:,:) = pagetranspose(RS)         ;
+    end
+
+    RA_  = zeros(N_,N_,ceil(N_/2))            ;
+    RS_  = zeros(N_,N_,ceil(N_/2))                   ;     
 
     for k = 1:1:ceil(N_/2)
         for j = 1:1:N_
             for i = 1:1:N_
                 if(D_(i,j,k))
                                                   
-                    RA_(i,j,k) = RA(D_(i,j,k),j);   
-                    RS_(i,j,k) = RS(D_(i,j,k),j);                
+                    RA_(i,j,k,h) = RA(D_(i,j,k),j,h);   
+                    RS_(i,j,k,h) = RS(D_(i,j,k),j,h);                
                 end   
             end
         end
     end
-    R_          = zeros(N_,N_,ceil(N_/2),2);
-    R_(:,:,:,1) = RA_                      ;
-    R_(:,:,:,2) = RS_                      ;
+
+    R_          = zeros(N_,N_,ceil(N_/2),2)           ;
+    R_(:,:,:,1) = RA_                                 ;
+    R_(:,:,:,2) = RS_                                 ;
