13c4427d48cc06097f9c91b8f65ed385714ff563
johnbodhi <61892807+johnbodhi@users.noreply.github.com>
2025-10-18 09:22:38 -0700

    Update parameterize.m

--- Project_Euler_Problem_67/MaximumPathSumII/MaximumPathSumII/MATLAB/parameterize.m
+++ Project_Euler_Problem_67/MaximumPathSumII/MaximumPathSumII/MATLAB/parameterize.m
@@ -4,14 +4,10 @@ function [R_] = parameterize(R_)
     % sub-space distribution of Lagrangians to use in p-adic space.
 
     % This will enable further efficiency by reducing the search space to
-    % fewer and lesser convolutional binary groups. 
+    % fewer and lesser convolutional binary groups, especially when the 
+    % search space is large.
 
     % Here, we are able to span forward and back ward linear sweeps 
-    % across all observations simultaneously.
-
-    
-
-
-
+    % across all observations almost simultaneously.
 
     
