d80b7ebc1d49cce3c2b1d9c169274768c973ec22
johnbodhi <61892807+johnbodhi@users.noreply.github.com>
2025-11-26 20:33:05 -0700

    Update

--- Project_Euler_Problem_67/MaximumPathSumII/MaximumPathSumII/MATLAB/allocate.m
+++ Project_Euler_Problem_67/MaximumPathSumII/MaximumPathSumII/MATLAB/allocate.m
@@ -1,29 +1,29 @@
-function [ F_, B_, STL_, STU_, SP_ ] = allocate( N_, Q_, MOD_ )
+function [F_,B_,A_,STL_,STU_,SP_] = allocate(N_,Q_,MOD_)
 
-    DATARANGE  = N_-floor(Q_/2);
+    DATARANGE  = N_-floor(Q_/2)                                              ;
 
-    GENERATORS = 4; 
+    GENERATORS = 4                                                           ; 
     
-    B_         = zeros(GENERATORS,N_-1);
+    B_         = zeros(GENERATORS,N_-1)                                      ;
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    B_(3,1:ceil(Q_/2),1:ceil(Q_/2)) = permn( (MOD_:-1:0)', ceil(Q_/2), 1 );
+    B_(3,1:ceil(Q_/2),1:ceil(Q_/2)) = permn( (MOD_:-1:0)', ceil(Q_/2), 1 )   ;
     
-    STL_               = (ceil(Q_/2):-1:2)';
+    STL_               = (ceil(Q_/2):-1:2)'                                  ;
 
-    STU_               = (DATARANGE:-1:ceil(Q_/2))';
+    STU_               = (DATARANGE:-1:ceil(Q_/2))'                          ;
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     % We need an initial condition in SP_ to avoid bit-skipping in the
     % DNN...
 
-    SP_                               = zeros( MOD_, DATARANGE );
+    SP_                               = zeros( MOD_, DATARANGE )             ;
 
-    SP_(B_(3,ceil(Q/2)),ceil(Q_/2)-1) = B_(3,ceil(Q_/2));
+    SP_(B_(3,ceil(Q/2)),ceil(Q_/2)-1) = B_(3,ceil(Q_/2))                     ;
 
-    B_(3,ceil(Q/2))                   = 0;
+    B_(3,ceil(Q/2))                   = 0                                    ;
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -33,4 +33,16 @@ function [ F_, B_, STL_, STU_, SP_ ] = allocate( N_, Q_, MOD_ )
     % We can use gradients to mimize our beliefs and expectations about the
     % future bits...
 
-    F_ = zeros( N_-1, DATARANGE );
+    F_ = zeros( N_-1, DATARANGE )                                            ;
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    % We can batch process the convolution of the directives for the
+    % decision tree...
+
+    A_ = zeros(N_)                                                           ;
+
+    for i = 1:1:N_
+
+        A_(i,1:ceil(i/2)) = 1                                                ;
+    end
